0af1ab825037e133c0a4ccb0328cce2617564215
Eonwake <142854385+Avid-Xenoblade-Player@users.noreply.github.com>
2024-06-03 16:06:05 +1000

    added files via upload

---
+++ main_output.py
@@ -0,0 +1,7 @@
+There are 37 stones. Each stone costs 20 anas to mine.
+It costs 740 anas total to produce these stones. The company now puts a markup of 10 on each stone, for a total of 30 anas per stone purhasing price.
+25 stones are sold. The company sells these stones for 750 anas total, for a profit of 10 anas.
+
+
+** Process exited - Return Code: 0 **
+Press Enter to exit terminal
