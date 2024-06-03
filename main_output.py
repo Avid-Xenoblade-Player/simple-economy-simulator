b6aa7a461cc267d82a56e64a025e5822e3575a96
Eonwake <142854385+Avid-Xenoblade-Player@users.noreply.github.com>
2024-06-03 17:00:30 +1000

    Update main_output.py
    first release soon..?

--- main_output.py
+++ main_output.py
@@ -9,8 +9,8 @@ companyMiningCost = stonesNumber * stonesMiningCost
 
 # Demand: Chance of a stone being sold (between 35% and 100%)
 demandRate = round(random.random() * 0.8 + 0.35, 2) 
-if demandRate > 1:
-    demandRate = 1
+if demandRate > 0.98:
+    demandRate = 0.98
     
 # Markup on the stones (fixed cost added to mining cost)
 stonesMarkup = round(stonesMiningCost / random.randint(2, 5))
@@ -26,10 +26,13 @@ print("There were", stonesNumber, "stones mined, each at a cost of", stonesMinin
       companyMiningCost, "anas.")
 print("The company adds a markup of", stonesMarkup, "anas, for a sale price of", stonesCost, "anas.")
 print("Out of the", stonesNumber, "stones,", stonesSold, "stones were sold, for a total sales of", companySales, "anas.")
-print("Demand was at a rate of",demandRate*100,"% today.")
+if demandRate > 0.95:
+    print("People really want these stones! Demandm was at a rate of",demandRate*100,"% today.")
+else:
+    print("Demand was at a rate of",demandRate*100,"% today.")
 if companyProfit < 0:
     print("Sales were bad. The company lost", companyProfit * -1, "anas.")
-elif 0 < companyProfit < companyMiningCost * 1.5:
+elif 0 < companyProfit < companyMiningCost * 1.25:
     print("Sales were okay. The company made", companyProfit, "anas.")
 else:
     print("Business is booming! The company made", companyProfit, "anas.")
